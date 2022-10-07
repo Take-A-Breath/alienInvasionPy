ad1c46cc285d8480578e76e4c2a2dc8bfa4ca959
JeremiahShafer <88599453+Take-A-Breath@users.noreply.github.com>
2022-10-06 22:07:04 -0500

    adding a single alien

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,7 @@ import pygame
 from settings import Settings
 from ship import Ship
 from bullet import Bullet
+from alien import Alien
 
 class AlienInvasion:
     """Overall class to manage game assets and behavior."""
@@ -19,6 +20,9 @@ class AlienInvasion:
 
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
+        self.aliens = pygame.sprite.Group()
+        
+        self._create_fleet()
 
     def run_game(self):
         """Start the main loop for the game."""
@@ -72,12 +76,19 @@ class AlienInvasion:
                 self.bullets.remove(bullet)
             print(len(self.bullets))
 
+    def _create_fleet(self):
+        """Create a fleet of aliens"""
+        # Make an alien
+        alien = Alien(self)
+        self.aliens.add(alien)
+    
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()
+        self.aliens.draw(self.screen)
 
         pygame.display.flip()
 
